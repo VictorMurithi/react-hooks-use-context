8d61c646832c019f704b8d7e3f624b7196e06060
victor <victor.murithi@student.moringaschool.com>
2023-11-03 09:20:17 +0300

    completed lab

---
+++ src/context/user.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react';
+
+const UserContext = React.createContext();
+function UserProvider({children}) {
+    const [user, setUser] = useState(null);
+    const currentUser = {
+        name: "Duane",
+        interests: ["Coding", "Biking", "Words ending in 'ing'"],
+      };
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+export {UserContext,UserProvider};
